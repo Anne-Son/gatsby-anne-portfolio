e8eaeb7ac5b06e73be9d41eed031c2219fbbf9b7
Anne Son <annesontech@gmail.com>
2021-06-11 18:40:17 -0700

    typewriter effect commit

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,6 +2,10 @@ import React, { useContext, useState, useEffect } from 'react';
 import { Container } from 'react-bootstrap';
 import Fade from 'react-reveal/Fade';
 import { Link } from 'react-scroll';
+
+import Typewriter from 'typewriter-effect';
+import BackgroundImage from 'gatsby-background-image';
+import { graphql, useStaticQuery } from 'gatsby';
 import PortfolioContext from '../../context/context';
 
 const Header = () => {
@@ -21,27 +25,68 @@ const Header = () => {
     }
   }, []);
 
+  const data = useStaticQuery(
+    graphql`
+      query {
+        desktop: file(relativePath: { eq: "heroBg.png" }) {
+          childImageSharp {
+            fluid(maxWidth: 1920) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    `
+  );
+  const imageData = data.desktop.childImageSharp.fluid;
+
   return (
     <section id="hero" className="jumbotron">
-      <Container>
-        <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={500} distance="30px">
-          <h1 className="hero-title">
-            {title || 'Hi, my name is'}{' '}
-            <span className="text-color-main">{name || 'Your Name'}</span>
-            <br />
-            {subtitle || "I'm the Unknown Developer."}
-          </h1>
-        </Fade>
-        <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
-          <p className="hero-cta">
-            <span className="cta-btn cta-btn--hero">
-              <Link to="about" smooth duration={1000}>
-                {cta || 'Know more'}
-              </Link>
-            </span>
-          </p>
-        </Fade>
-      </Container>
+      <BackgroundImage className="hero-background" fluid={imageData}>
+        <div className="sky-container">
+          <div className="star" />
+          <div className="star" />
+          <div className="star" />
+          <div className="star" />
+          <div className="star" />
+        </div>
+        <Container>
+          <div className="me">
+            <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={500} distance="30px">
+              <h1 className="hero-title">
+                {title || 'Hi, my name is'}{' '}
+                <span className="text-color-main">{name || 'Hyu Son'}</span>
+                <br />
+                <span className="call-me">
+                  (You can call me <span className="text-color-main">Anne</span> )
+                </span>
+                {subtitle || (
+                  <Typewriter
+                    onInit={(typewriter) => {
+                      typewriter
+                        .typeString('I am a Web Developer')
+                        .pauseFor(200)
+                        .deleteChars(13)
+                        .typeString('Front-End Developer')
+                        .pauseFor(1000)
+                        .start();
+                    }}
+                  />
+                )}
+              </h1>
+            </Fade>
+            <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
+              <p className="hero-cta">
+                <span className="cta-btn cta-btn--hero">
+                  <Link to="about" smooth duration={1000}>
+                    {cta || 'Know more'}
+                  </Link>
+                </span>
+              </p>
+            </Fade>
+          </div>
+        </Container>
+      </BackgroundImage>
     </section>
   );
 };
